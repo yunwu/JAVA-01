fb064d145ff61b43cc304cfa240b4a5264b459d4
wangdan1 <wangdan1@jinalc.com>
2021-02-07 12:16:32 +0800

    demo5提交

--- Week_04/cocurrent-demo/src/main/java/com/cocurrent/Demo4.java
+++ Week_04/cocurrent-demo/src/main/java/com/cocurrent/Demo4.java
@@ -1,23 +1,45 @@
 package com.cocurrent;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
+/**
+ * List<Callable<String>> 实现
+ */
 public class Demo4 {
 
     public static void main(String[] args){
 
-        ExecutorService pool = new ThreadPoolExecutor(1, 1, 600, TimeUnit.SECONDS, new ArrayBlockingQueue<>(10));
-
-        Future<String> future = pool.submit(new Callable<String>() {
+        List<Callable<String>> task = new ArrayList<>();
+        task.add(new Callable<String>() {
             @Override
             public String call() throws Exception {
-                return "hello world";
+                return "hello world!";
             }
         });
-        try{
-            System.out.println(future.get(1000, TimeUnit.MILLISECONDS));
-        }catch (InterruptedException | ExecutionException | TimeoutException ex){
-            ex.printStackTrace();
+        task.add(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "hello King";
+            }
+        });
+        task.add(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "hello lili";
+            }
+        });
+
+        ExecutorService pool = Executors.newFixedThreadPool(1);
+        try {
+            List<Future<String>> results = pool.invokeAll(task);
+            for (Future<String> result : results) {
+               System.out.println(result.get());
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
+
     }
 }
---
+++ Week_04/cocurrent-demo/src/main/java/com/cocurrent/Demo5.java
@@ -0,0 +1,27 @@
+package com.cocurrent;
+
+import java.util.concurrent.*;
+import java.util.function.Supplier;
+
+/**
+ * CompletableFuture 实现，分析一下completableFuture
+ */
+public class Demo5 {
+
+    public static void main(String[] args){
+
+        ExecutorService pool = Executors.newFixedThreadPool(1);
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(new Supplier<String>() {
+            @Override
+            public String get() {
+                return "hello world!";
+            }
+        }, pool);
+
+        try{
+            System.out.println(future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
